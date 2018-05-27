abb402151d4b942ffd5ccdfd91be75c801f541e0
Anthony Pasquariello <antpas@bu.edu>
2018-05-27 16:22:05 -0400

    fixed port

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/', (req,res) => {
 });
 
 //Listen to port 3000
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Starting the server at port ${port}`);
 });
