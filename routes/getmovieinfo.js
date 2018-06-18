f5560942a5c176ba592d2543cba922236de1e52e
Anthony Pasquariello <antpas@bu.edu>
2018-06-18 11:54:45 -0400

    added genre functionality and modularized code

--- routes/getmovieinfo.js
+++ routes/getmovieinfo.js
@@ -2,21 +2,40 @@
 const express = require('express');
 const http = require('http');
 const imdb = require('imdb-api');
-let moviedbKey = process.env.MOVIE_DB_KEY
+const moviedbKey = process.env.MOVIE_DB_KEY
 const MovieDB = require('moviedb')(moviedbKey);
 const bodyParser = require('body-parser');
 const router = express.Router();
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY
 const os = require('os');
+const  request = require('request');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: true
 }));
 
+function returnMultMovies(movies) {
+    let listMovies = ""
+    for(let i=0; i < movies.results.length; i++){
+        if(!movies.results[i].hasOwnProperty('year'))
+            listMovies = listMovies + " " + movies.results[i].title + ", "
+        else
+            listMovies = listMovies + " " + movies.results[i].title + " " + "(" + movies.results[i].year + "), " 
+
+    }
+    outText = "How about these?" + listMovies
+    output = 
+    {
+        "fulfillmentText": outText,
+    }
+    return output
+ } 
+
 router.post('/', (req,res) => {
     
     let action = req.body.queryResult.action;
+    let parameter = req.body.queryResult.parameters
     let outText;
     let output;
     let movieInput;
@@ -25,8 +44,8 @@ router.post('/', (req,res) => {
     if(action == "getmovieinfo"){
         movieInput = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
         imdb.get(movieInput, {apiKey: MOVIE_API_KEY, timeout: 30000}).then(movie => {
-            outText = movie.title + " was released in " + movie.year + " and directed by " + movie.director + ". " +
-            movie.title + " was " + movie.awards;
+            outText = movie.title + " was released in " + movie.year + " and directed by " + movie.director + 
+            ". The metascore is: " + movie.metascore + "%";
             output = 
             {
                 "fulfillmentText": outText,
@@ -44,8 +63,64 @@ router.post('/', (req,res) => {
         });
     }
     else if(action == "popular"){
-        output = "popular"
-        res.json(output)
+
+        MovieDB.miscPopularMovies((err, moviedbResp) => {
+            let output = returnMultMovies(moviedbResp)
+            res.json(output)
+        });
+    }
+    else if(action == "genre"){
+        let apiURL = "https://api.themoviedb.org/3/discover/movie?api_key=" + moviedbKey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="
+        if(parameter.romance == "romance"){
+            apiURL = apiURL + '10749' //romance id
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else if(parameter.drama == "drama"){
+            apiURL = apiURL + '18'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else if(parameter.action == "action"){
+            apiURL = apiURL + '28'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else if(parameter.comedy == "comedy"){
+            apiURL = apiURL + '35'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else if(parameter.documentary == "documentary"){
+            apiURL = apiURL + '99'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else if(parameter.scifi == "scifi"){
+            apiURL = apiURL + '878'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+        }
+        else{
+            apiURL = apiURL + '12'
+            request(apiURL, function (error, response, movie) {
+                output = returnMultMovies(JSON.parse(movie))                
+                res.json(output)
+            });
+
+        }
     }
     else if(action == "search"){
         output = "search"
@@ -58,15 +133,7 @@ router.post('/', (req,res) => {
           }, {
             apiKey: MOVIE_API_KEY
           }).then(movie => {
-                let listMovies = ""
-                for(let i=0; i< movie.results.length; i++){
-                    listMovies = listMovies + " " + movie.results[i].title + " " + "(" + movie.results[i].year + "), " 
-                }
-                outText = "Which one of these?" + listMovies
-                output = 
-                {
-                    "fulfillmentText": outText,
-                }
+                let output = returnMultMovies(movie)
                 res.json(output)
         });
     }
